92df2b61213b039b57ca2125a4d22feb9285c568
kimjs98 <zokj0691@naver.com>
2022-03-28 17:30:13 +0900

    11727

---
+++ backjoon/11727_2n_tiling2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n; 
+	vector <int> d(n+1);
+	
+	d[1] = 1;
+	d[2] = 3;
+	
+	for(int i = 3; i < n+1; i++ )
+	{
+		d[i] = ( d[i-1] + 2*d[i-2] )%10007 ;	
+	}
+	cout << d[n] << '\n';
+	
+	return 0;
+}
