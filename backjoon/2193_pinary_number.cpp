4bb9b68881eb2e71365414c5e6b783ac178b3541
kimjs98 <zokj0691@naver.com>
2023-02-22 00:00:54 +0900

    2193

---
+++ backjoon/2193_pinary_number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define MAX 91
+using namespace std;
+
+long long dp[MAX][2];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n;
+	cin >> n;
+
+	dp[1][1] = 1;
+	dp[2][0] = 1;
+	
+	for(int i=3; i<=n; i++) {
+		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+		dp[i][1] = dp[i - 1][0]; 
+	}
+	
+	cout << dp[n][0] + dp[n][1];
+
+	return 0;
+}
